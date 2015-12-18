d03e0378b95723d5220de1419a25a546abbafab1
Sayeed Anjum <sayeed@greytip.com>
2015-12-18 10:17:50 +0530

    suppress echoing credentials to stdout

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 
 # SIGTERM-handler
 term_handler() {
@@ -8,10 +7,13 @@ term_handler() {
 
 trap 'kill ${!}; term_handler' SIGTERM
 
-sed -ie "s,ES_ENDPOINT,${ES_ENDPOINT},g" /etc/td-agent/td-agent.conf
-sed -ie "s/ES_REGION/${ES_REGION}/g" /etc/td-agent/td-agent.conf
 sed -ie "s/ES_AWS_ACESS_KEY_ID/${ES_AWS_ACCESS_KEY_ID}/g" /etc/td-agent/td-agent.conf
 sed -ie "s/ES_AWS_SECRET_ACCESS_KEY/${ES_AWS_SECRET_ACCESS_KEY}/g" /etc/td-agent/td-agent.conf
+
+set -x
+
+sed -ie "s,ES_ENDPOINT,${ES_ENDPOINT},g" /etc/td-agent/td-agent.conf
+sed -ie "s/ES_REGION/${ES_REGION}/g" /etc/td-agent/td-agent.conf
 
 
 /usr/sbin/td-agent $FLUENTD_ARGS -o /var/log/td-agent/td-agent.log 
