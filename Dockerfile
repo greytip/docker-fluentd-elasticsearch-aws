a4f026aa56dce55ba454e945a751ee140be5d4a4
Sayeed Anjum <sayeed@greytip.com>
2015-12-18 08:16:06 +0530

    added proper config file as part of the image

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM organizeme/docker-fluentd-elasticsearch-aws
 RUN /usr/sbin/td-agent-gem install fluent-plugin-docker_metadata_filter
 RUN /usr/sbin/td-agent-gem install fluent-plugin-ec2-metadata
 
+COPY td-agent.conf /etc/td-agent/td-agent.conf
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
 
-# Create Logstash index template & run the Fluentd service.
+# Create fluentd index template & run the Fluentd service.
 CMD ["/start.sh"]
 
--- start.sh
+++ start.sh
@@ -8,5 +8,11 @@ term_handler() {
 
 trap 'kill ${!}; term_handler' SIGTERM
 
+sed -ir "s/ES_ENDPOINT/$ES_ENDPOINT/g" /etc/td-agent/td-agent.conf
+sed -ir "s/ES_REGION/$ES_REGION/g" /etc/td-agent/td-agent.conf
+sed -ir "s/AWS_ACESS_KEY_ID/$AWS_ACCESS_KEY_ID/g" /etc/td-agent/td-agent.conf
+sed -ir "s/AWS_SECRET_ACCESS_KEY/$AWS_SECRET_ACCESS_KEY/g" /etc/td-agent/td-agent.conf
+
+
 /usr/sbin/td-agent $FLUENTD_ARGS -o /var/log/td-agent/td-agent.log 
 
